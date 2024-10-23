8da49285588d1ccb6148cf15080b6c324e43e137
Kibi <kibiandkimi@outlook.com>
2024-10-23 11:50:45 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::process::{Command};
 use std::env;
 use std::fs;
+use std::path::{Path};
 use yaml_rust2::{YamlLoader};
 use chrono::prelude::*;
 
@@ -13,10 +14,13 @@ fn main() {
 
 fn start() {
     let args: Vec<String> = env::args().collect();
-    println!("{}", args[0]);
     let file_path = (&args[1]).clone();
 
-    let raw_config = fs::read_to_string("config.yml")
+    let mut config_path = Path::new(&args[0]);
+    config_path = config_path.parent().unwrap();
+    let config_path = config_path.join("config.yml");
+
+    let raw_config = fs::read_to_string(config_path)
         .expect("Can't read file config.yml!");
 
     let config = YamlLoader::load_from_str(raw_config.as_str())
